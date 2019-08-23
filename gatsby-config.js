1d229b720e234b57fb3a7641f9a65b3b822d2ba9
al <alpiepho@yahoo.com>
2019-08-23 12:11:07 -0600

    added workaround for first about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,6 +94,15 @@ module.exports = {
       resolve: `gatsby-plugin-create-client-paths`,
       options: { prefixes: [`/scores/*`] },
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
